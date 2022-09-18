18428a8f3a8e4f426e4517ea8afe163c510f88da
slakingex@gmail.com <slakingex@gmail.com>
2022-09-17 17:18:07 -0700

    added lower() to input keyword)

--- app.py
+++ app.py
@@ -1,35 +1,90 @@
+from math import prod
 import os
-
+import numpy as np
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+product_list = []
+with open("./distinct_product_types.csv", encoding='utf-8-sig') as product_type_file: 
+    for line in product_type_file.readlines(): 
+        product_list.append(line.replace("_"," ").lower().strip("\n"))
+
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        animal = request.form["animal"].lower()
         response = openai.Completion.create(
             model="text-davinci-002",
             prompt=generate_prompt(animal),
             temperature=0.6,
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        generated_type_keywords = response.choices[0].text.split(",")
+        product_list_for_embedding = product_list + generated_type_keywords
+        index_for_keyword = product_list_for_embedding.index(animal)
+        # print(product_list_for_embedding)
+
+        embedding_vector = openai.Embedding.create(
+            input = product_list_for_embedding,
+            engine="text-similarity-davinci-001"
+        )
+        valid_results = []
+        valid_index= []
+        
+        embedding_vector_for_generated = embedding_vector['data'][-5: None :1]
+        embedding_vector_for_amazon = embedding_vector['data'][0:-5:1]
+        
+        # Get the closest amazon product type 
+        for vector in embedding_vector_for_generated :
+            print("starting loop for similarity")
+            max_score = -10000
+            for amzn_vector_num in range(len(embedding_vector_for_amazon)): 
+                similarity_score = np.dot(embedding_vector_for_amazon[amzn_vector_num]['embedding'],vector['embedding'])
+                print(f"similarity_score {similarity_score}")
+                if similarity_score>max_score:
+                    if product_list[amzn_vector_num]==animal: 
+                        pass 
+                    else: 
+                        max_score=similarity_score
+                        print(f"max_score{max_score}")
+                        valid_results.append(product_list[amzn_vector_num])
+                        valid_index.append(amzn_vector_num)
+                        print(product_list[amzn_vector_num])
+
+        # remove duplicates from list 
+        valid_results_without_dup = [*set(valid_results)]
+        valid_index_without_dup = [*set(valid_index)]
+
+        #get the amazon product types that are closest to original keywords 
+        candid_score_dict = {}
+        for index in valid_index_without_dup: 
+            similarity_score = np.dot(embedding_vector_for_amazon[index]['embedding'], embedding_vector['data'][index_for_keyword]['embedding'])
+            candid_score_dict[index] = similarity_score
+        
+        sorted_candid_score_dict =dict(sorted(candid_score_dict.items(),key=lambda item: item[1]))
+        for final_index in sorted_candid_score_dict.keys(): 
+            valid_results.append(product_list[final_index])
+            
+        response_text = response.choices[0].text + f"\n valid amazon types : {valid_results[:5]}" 
+        return redirect(url_for("index", result=response_text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
 def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+    return """Suggest five product types that are complementary to the suggested product type. 
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
+Product type:  bread 
+Complementary product types: cheese, butter, jam, butterknife, toaster
+Product type:  bed 
+Complementary product types: pillow, blanket, bedsheet, mattress, bed frame 
+Product type:  cups
+Complementary product types: dishes, forks, knives, cutlery 
+Product type: {}
+Complementary product types: """.format(
         animal.capitalize()
     )
